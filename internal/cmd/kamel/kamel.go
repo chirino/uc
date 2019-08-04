f710f82249550a7ca23f46a7bf0aa1d6e124ac50
Hiram Chirino <hiram@hiramchirino.com>
2019-08-04 12:03:39 -0400

    Cleanup

--- internal/cmd/kamel/kamel.go
+++ internal/cmd/kamel/kamel.go
@@ -4,7 +4,6 @@ package kamel
 import (
     "github.com/chirino/uc/internal/cmd"
     "github.com/spf13/cobra"
-    "k8s.io/apimachinery/pkg/version"
     "k8s.io/client-go/kubernetes"
 )
 
@@ -12,19 +11,9 @@ func init() {
     cmd.SubCmdFactories = append(cmd.SubCmdFactories, NewCmd)
 }
 
-func NewCmd(options *cmd.Options, api *kubernetes.Clientset, info *version.Info) (*cobra.Command, error) {
-
-    // Todo: figure out best way select the best client version for the server we are connected to.
-    serverToClientVersionMap := map[string]string{
-        "3.11.0": "1.0.0-M1",
-        "1.12+":  "1.0.0-M1",
-    }
-    serverVersion := info.Major + "." + info.Minor
-    clientVersion := serverToClientVersionMap[serverVersion]
-    if clientVersion == "" {
-        return nil, nil
-    }
-
+func NewCmd(options *cmd.Options, api *kubernetes.Clientset) (*cobra.Command, error) {
+    // Todo: figure out how to pick the best client version for the server we are connected against.
+    clientVersion := "latest"
     return cmd.GetCobraCommand(options, "kamel", clientVersion)
 }
 
