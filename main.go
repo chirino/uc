c316820424f472aef6d229c6303257e2cbd4d53e
Hiram Chirino <hiram@hiramchirino.com>
2019-08-06 17:54:15 -0400

    Pick up hawtgo module update so we can use an exec call to the subcommands.

--- main.go
+++ main.go
@@ -1,44 +1,47 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/chirino/uc/internal/cmd"
-    _ "github.com/chirino/uc/internal/cmd/kamel"
-    _ "github.com/chirino/uc/internal/cmd/kubectl"
-    _ "github.com/chirino/uc/internal/cmd/updatecat"
-    _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-    "math/rand"
-    "os"
-    "path/filepath"
-    "strings"
-    "time"
+	"context"
+	"fmt"
+	"github.com/chirino/uc/internal/cmd"
+	_ "github.com/chirino/uc/internal/cmd/kamel"
+	_ "github.com/chirino/uc/internal/cmd/kubectl"
+	_ "github.com/chirino/uc/internal/cmd/updatecat"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 func main() {
-    rand.Seed(time.Now().UTC().UnixNano())
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel() // Cancel ctx as soon as main returns
+	rand.Seed(time.Now().UTC().UnixNano())
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // Cancel ctx as soon as main returns
 
-    cmd, err := cmd.New(ctx)
-    ExitOnError(err)
+	cmd, err := cmd.New(ctx)
+	ExitOnError(err)
 
-    exeName := filepath.Base(os.Args[0])
-    if !strings.HasPrefix(exeName, "___go_build_") {
-        cmd.Use = exeName
-    }
-    // First time discovers sub commands..
-    err = cmd.Execute()
-    ExitOnError(err)
+	// in case our binary gets renamed, use that in
+	// our help/usage screens.
+	// unless it's because it's being run by `go run ...`
+	exeName := filepath.Base(os.Args[0])
+	if !strings.HasPrefix(exeName, "___go_build_") {
+		cmd.Use = exeName
+	}
+	// the first time we execute the root command, it discovers / dynamically sets up the sub commands
+	err = cmd.Execute()
+	ExitOnError(err)
 
-    // Second time the sub-commands will be setup..
-    err = cmd.Execute()
-    ExitOnError(err)
+	// the second time we execute, control gets passed to the sub commands.
+	err = cmd.Execute()
+	ExitOnError(err)
 }
 
 func ExitOnError(err error) {
-    if err != nil {
-        fmt.Println("error:", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 }
