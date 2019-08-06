4ec1e5103c44adaefbb96039ccff415c72d716e3
Hiram Chirino <hiram@hiramchirino.com>
2019-08-06 16:22:01 -0400

    fix: don't fail if we can't connect to the api server, just go with the latest versions of the sub commands.

--- internal/cmd/kubectl/kubectl.go
+++ internal/cmd/kubectl/kubectl.go
@@ -2,25 +2,23 @@
 package kubectl
 
 import (
-    "github.com/chirino/uc/internal/cmd"
-    "github.com/spf13/cobra"
-    "k8s.io/client-go/kubernetes"
-    "strings"
+	"github.com/chirino/uc/internal/cmd"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+	"strings"
 )
 
 func init() {
-    cmd.SubCmdFactories = append(cmd.SubCmdFactories, NewCmd)
+	cmd.SubCmdFactories = append(cmd.SubCmdFactories, NewCmd)
 }
 
 func NewCmd(options *cmd.Options, api *kubernetes.Clientset) (*cobra.Command, error) {
-    clientVersion := "latest"
-    if api!=nil {
-        info, err := api.ServerVersion()
-        if err != nil {
-            return nil, err
-        }
-        clientVersion = strings.Split(strings.TrimPrefix(info.GitVersion, "v"), "-")[0]
-    }
-    return cmd.GetCobraCommand(options, "kubectl", clientVersion)
+	clientVersion := "latest"
+	if api != nil {
+		info, err := api.ServerVersion()
+		if err == nil {
+			clientVersion = strings.Split(strings.TrimPrefix(info.GitVersion, "v"), "-")[0]
+		}
+	}
+	return cmd.GetCobraCommand(options, "kubectl", clientVersion)
 }
-
