03ac6a5b2e6c3335e2938e90f319fa8220a96031
Hiram Chirino <hiram@hiramchirino.com>
2019-08-10 15:00:32 -0400

    chore: added a command to find new uc releases.
    Updated the catalog.

--- internal/cmd/catalog/find/find.go
+++ internal/cmd/catalog/find/find.go
@@ -9,6 +9,7 @@ import (
     "github.com/chirino/uc/internal/cmd/catalog/find/kubectl"
     "github.com/chirino/uc/internal/cmd/catalog/find/oc"
     "github.com/chirino/uc/internal/cmd/catalog/find/odo"
+    "github.com/chirino/uc/internal/cmd/catalog/find/uc"
     "github.com/spf13/cobra"
 )
 
@@ -22,5 +23,6 @@ func New(o *cmd.Options) *cobra.Command {
     command.AddCommand(odo.New(o))
     command.AddCommand(kamel.New(o))
     command.AddCommand(kn.New(o))
+    command.AddCommand(uc.New(o))
     return command
 }
