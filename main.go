749befdc17fca018f3a03100df8ee341cd49e03d
Hiram Chirino <hiram@hiramchirino.com>
2019-08-16 14:42:12 -0400

    feat: support symbolic linking the uc command to a different command name like oc or kubectl so that it can replace those commands.

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -21,6 +22,13 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel() // Cancel ctx as soon as main returns
+
+	// Does it look like a user linked a sub command to the uc command?
+	// ex: ln -s /usr/local/bin/uc /usr/local/bin/oc
+	commandName := filepath.Base(os.Args[0])
+	if commandName != "uc" {
+		os.Args = append([]string{"uc"}, os.Args...)
+	}
 
 	cmd, err := uc.New(&cmd.Options{
 		Context: ctx,
